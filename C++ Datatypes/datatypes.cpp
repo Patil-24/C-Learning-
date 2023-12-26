ac1d66f288dac52b22d158f6ce853be1ad3830f5
Iranna Patil <patiliranna24@gmail.com>
2023-12-26 23:41:26 +0530

    updated datatypes

--- C++ Datatypes/datatypes.cpp
+++ C++ Datatypes/datatypes.cpp
@@ -1,7 +1,17 @@
+// C++ Program to Demonstrate the correct size
+// of various data types on your computer.
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    int val = 10;
+    cout << "Size of char : " << sizeof(char) << endl;
+    cout << "Size of int : " << sizeof(int) << endl;
+
+    cout << "Size of long : " << sizeof(long) << endl;
+    cout << "Size of float : " << sizeof(float) << endl;
+
+    cout << "Size of double : " << sizeof(double) << endl;
+
+    return 0;
 }
